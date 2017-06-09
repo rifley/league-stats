e6772d75c9cc096d1b6b53e705a0e7855a33e3b8
Riley Watts <rileywatts.yes@gmail.com>
2017-06-09 14:25:16 -0700

    add user create page, login page, tie in with firebase auth

--- app/src/main/java/com/epicodus/leaguestats/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/leaguestats/ui/MainActivity.java
@@ -2,8 +2,12 @@ package com.epicodus.leaguestats.ui;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -14,6 +18,8 @@ import android.widget.TextView;
 
 import com.epicodus.leaguestats.Constants;
 import com.epicodus.leaguestats.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
@@ -23,22 +29,72 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.getStartedButton) Button mUserButton;
     @Bind(R.id.leagueTextView) TextView mLeagueTextView;
+    @Bind(R.id.summonerNameEditText) AutoCompleteTextView mSummonerName;
 
-    @Bind(R.id.summonerNameEditText)
-    AutoCompleteTextView mSummonerName;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Typeface leagueFont = Typeface.createFromAsset(getAssets(), "fonts/league.ttf");
-
         ButterKnife.bind(this);
 
+        Typeface leagueFont = Typeface.createFromAsset(getAssets(), "fonts/league.ttf");
         mUserButton.setOnClickListener(this);
         mLeagueTextView.setTypeface(leagueFont);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    getSupportActionBar().setTitle("Welcome, " + user.getDisplayName() + "!");
+                } else {
+
+                }
+            }
+        };
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
 
 
     @Override
