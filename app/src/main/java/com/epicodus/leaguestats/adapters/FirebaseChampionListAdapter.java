0cffb81a336407ae93923b8da2b59e7de2072a3f
riley watts <rileywatts.yes@gmail.com>
2017-06-15 13:50:20 -0700

    remove view from firebase on gesture dismiss

--- app/src/main/java/com/epicodus/leaguestats/adapters/FirebaseChampionListAdapter.java
+++ app/src/main/java/com/epicodus/leaguestats/adapters/FirebaseChampionListAdapter.java
@@ -30,12 +30,13 @@ public class FirebaseChampionListAdapter extends FirebaseRecyclerAdapter<Champio
 
     @Override
     public boolean onItemMove(int fromPosition, int toPosition) {
+        notifyItemMoved(fromPosition, toPosition);
         return false;
     }
 
     @Override
     public void onItemDismiss(int position) {
-
+        getRef(position).removeValue();
     }
 
     @Override
